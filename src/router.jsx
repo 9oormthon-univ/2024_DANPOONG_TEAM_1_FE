f45c973a5f322e6b53830aa64135821c9fa55a30
rryunn <rryunn@ajou.ac.kr>
2024-11-18 22:19:08 +0900

    router 경로 추가

--- src/router.jsx
+++ src/router.jsx
@@ -2,7 +2,14 @@ import { createBrowserRouter } from 'react-router-dom';
 import App from './App';
 import MainPage from './pages/main/MainPage';
 import NotFound from './pages/error/NotFound';
-
+import Login from './pages/login/Login';
+import LoginOption from './pages/login/LoginOption';
+import LoginForm from './components/login/LoginForm';
+import IndividualSignUp from './pages/signup/IndividualSignUp';
+import CompanySignUp from './pages/signup/CompanySignUp';
+import SignUpComplete from './components/login/SignUpComplete';
+import ForgotPasswdIndividual from './pages/login/ForgotPasswdIndividual';
+import ForgotPasswdCompany from './pages/login/ForgotPasswdCompany';
 const router = createBrowserRouter([
   {
     path: '/',
@@ -11,6 +18,40 @@ const router = createBrowserRouter([
       {
         path: '',
         element: <MainPage />,
+      },
+      {
+        path: 'login',
+        element: <Login />,
+        children: [
+          {
+            index: true,
+            element: <LoginOption />, // 기본 로그인 옵션 화면
+          },
+          {
+            path: 'form/:userType', // userType을 URL 파라미터로 받음
+            element: <LoginForm />, // 로그인 폼 화면
+          },
+          {
+            path: 'individual-signup',
+            element: <IndividualSignUp />, // 개인 회원가입 화면
+          },
+          {
+            path: 'company-signup',
+            element: <CompanySignUp />, // 기업 회원가입 화면
+          },
+          {
+            path: 'signup-complete',
+            element: <SignUpComplete />,
+          },
+          {
+            path: 'forgot-password-individual',
+            element: <ForgotPasswdIndividual />, // 개인 비밀번호 찾기
+          },
+          {
+            path: 'forgot-password-company',
+            element: <ForgotPasswdCompany />, // 기업 비밀번호 찾기
+          },
+        ],
       },
     ],
     errorElement: <NotFound />,
