c79c434981f6a878faa6fd728ae08dccda6b2bf0
jungsun <dlwjdtjs0111@gmail.com>
2024-11-18 15:44:30 +0900

    fix: 오타 수정

--- src/pages/main/MainPage.jsx
+++ src/pages/main/MainPage.jsx
@@ -6,7 +6,10 @@ import PlanList from '../../components/main/PlanList';
 function MainPage() {
   return (
     <>
+      <Header />
       <S.MainWrapper>
+        <S.Banner />
+        <PlanList />
       </S.MainWrapper>
     </>
   );
