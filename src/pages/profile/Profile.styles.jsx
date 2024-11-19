29f776efc57a197b6b230bd7a687fb4301ed70a7
rryunn <rryunn@ajou.ac.kr>
2024-11-20 00:01:54 +0900

    개인 프로필 페이지 생성

--- src/pages/profile/Profile.styles.jsx
+++ src/pages/profile/Profile.styles.jsx
@@ -1,1 +1,34 @@
-import { styled } from 'styled-components';
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+`;
+
+export const Title = styled.h1`
+  font-size: 2rem;
+  margin-bottom: 1rem;
+`;
+
+export const Content = styled.p`
+  font-size: 1.2rem;
+  margin-bottom: 2rem;
+`;
+
+export const LogoutButton = styled.button`
+  padding: 10px 20px;
+  font-size: 1rem;
+  color: #fff;
+  background-color: #9a50f1;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: #64369e;
+  }
+`;
