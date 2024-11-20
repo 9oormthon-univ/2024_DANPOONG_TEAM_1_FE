75b6cace9a2cea743e333fee0cd6ee840cd9297c
jungsun <dlwjdtjs0111@gmail.com>
2024-11-20 19:02:40 +0900

    🐛 fix: 에러 수정

--- src/components/planDetail/PlanMainDetail.jsx
+++ src/components/planDetail/PlanMainDetail.jsx
@@ -7,7 +7,7 @@ import planCommentIcon from '../../assets/images/plan-comment-icon.svg';
 import Planner from './components/Planner';
 import kakaoMapIcon from '../../assets/images/kakaomap-icon.svg';
 
-function PlanMainDetail(findPathRef) {
+function PlanMainDetail({ findPathRef }) {
   const currentPlan = useSelector(state => state.plan.currentPlan);
   const [categoryIcon, setCategoryIcon] = useState(null);
   const [categoryTitle, setCategoryTitle] = useState('');
